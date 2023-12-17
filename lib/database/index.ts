0e2e0bc956d7c78b84f362af861baa778f43f31b
Tamoor Ali <tamoorali35@gmail.com>
2023-12-17 11:23:43 +0500

    third commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,21 +1,20 @@
-import mongoose from 'mongoose'
-import { cache } from 'react';
+import mongoose from 'mongoose';
+
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// mongodb database username FinalYearProject pass Project123
-//creating connection to database
-let cached = (global as any ).mongoose || {con: null, promise: null};
-//  we are using cached connection to connect once to the database 
-// if we don't use use this then for every request made to server new connection will be required which is inefficient 
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
 export const connectToDatabase = async () => {
-    if(cached.conn) return cached.connect;
-    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'evently',
-        bufferCommands: false,
-    })
+  if (cached.conn) return cached.conn;
 
-    cached.con = await cached.promise;
-    return cached.conn;
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
